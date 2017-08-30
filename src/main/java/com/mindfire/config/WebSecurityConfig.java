b02446619ca7248768dd7392ff714a9b9c73f375
anant-dev <anantsingh1010@gmail.com>
2017-08-30 17:51:04 +0530

    initial commit

--- src/main/java/com/mindfire/config/WebSecurityConfig.java
+++ src/main/java/com/mindfire/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ package com.mindfire.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,26 +22,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @ComponentScan(basePackages = "com.mindfire")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("user").roles("USER");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest()
-//                .fullyAuthenticated()
-//                .and().httpBasic()
-//                .and()
-//                .formLogin().loginPage("/login").failureUrl("/login?error")
-//                .usernameParameter("username").passwordParameter("password")
-//                .and()
-//                .logout().logoutSuccessUrl("/login?logout");
 
         http.authorizeRequests()
-                .antMatchers("/empform", "/viewemp", "/editemp/**", "/editsave", "/deleteemp/*").access("hasRole('ROLE_USER')")
+                .antMatchers("/empform", "/viewemp", "/editemp/**", "/editsave", "/deleteemp/*").authenticated()
                 .and()
                 .formLogin().loginPage("/login").failureUrl("/login?error")
                 .usernameParameter("username").passwordParameter("password")
@@ -48,5 +39,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutSuccessUrl("/login?logout");
         http.csrf().disable();
     }
+    
+    
+     @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+
+            authenticationManagerBuilder
+                    .ldapAuthentication()
+                    .userSearchFilter("(uid={0})")
+                    .userSearchBase("ou=people")
+                    .groupSearchFilter("uniqueMember={0}")
+                    .groupSearchBase("ou=users,ou=groups")
+                    .userDnPatterns("uid={0},ou=people")
+                    .rolePrefix("ROLE_")
+                    .contextSource()
+                    .url("ldap://localhost:10389/o=ldapsample")
+                    .managerPassword("secret").managerDn("uid=admin,ou=system");        }
+    }
+    
 
 }
